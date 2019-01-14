8c2065c6e79b4c9dced3b7438c1b08d18333ec71
szapp <dev.szapp@gmail.com>
2019-01-14 22:06:03 +0100

    Send built information to zSpy on initialization

--- src/data/console.asm
+++ src/data/console.asm
@@ -1,4 +1,5 @@
-NINJA_VERSION_CHAR_1     db   'Ninja ', NINJA_VERSION, ' (built ', __UTC_DATE__, ' ', __UTC_TIME__, ', ', 0
+NINJA_VERSION_CHAR_1     db   'Ninja ', NINJA_VERSION, ' ('
+NINJA_VERSION_CHAR_built db   'built ', __UTC_DATE__, ' ', __UTC_TIME__, ', ', 0
 NINJA_VERSION_CHAR_1_len equ  $-NINJA_VERSION_CHAR_1
 NINJA_VERSION_CHAR_2     db   ') <', NINJA_WEBSITE, '>', 0
 NINJA_CON_COMMAND        db   'NINJA', 0
